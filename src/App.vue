b105abb8000f8a148f37fe586ca6187644480291
vegin <734385318@qq.com>
2019-11-29 11:05:35 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <p>
         If Element is successfully added to this project, you'll see an
         <code v-text="'<el-button>'"></code>
-        below
+        below 测个试
       </p>
       <el-button>el-button</el-button>
     </div>
