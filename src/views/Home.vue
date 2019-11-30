9caf7bf85d6bcdace2dcde9216dc9493e2b97ffd
vegin <734385318@qq.com>
2019-11-30 16:59:41 +0800

    element组件主题调整

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <el-button type='primary' @click="changeTheme">el-button</el-button>
-    <p :style="{'color':Theme.primaryColor}">测试theme</p>
+    <el-button type='primary'>el-button</el-button>
+    <p class="test">再测试一下</p>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -10,21 +10,16 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-import Theme from '../theme/index.js'
 export default {
   name: 'home',
   components: {
     HelloWorld
-  },
-  data(){
-    return{
-      Theme
-    }
-  },
-  methods:{
-    changeTheme:()=>{
-      Theme.set('default')
-    }
   }
 }
 </script>
+<style lang="scss">
+@import '@/assets/css/element-variables.scss';
+.test{
+  color:$--color-primary;
+}
+</style>
